62a76040628407acafc7ed6eb734542d353f6b0f
Billy Yang <handsomexdd1024@gmail.com>
2023-03-17 22:07:46 +0800

    Add lab02 (percolation) files

---
+++ hello/RandomWord.java
@@ -0,0 +1,27 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String word = "", tmp;
+        int count = 0;
+        //        while (true) {
+        //            try {
+        //                tmp = StdIn.readString();
+        //                if (StdRandom.bernoulli(1.0/(++count))) {
+        //                    word = tmp;
+        //                }
+        //            } catch (NoSuchElementException e) {
+        //                break;
+        //            }
+        //        }
+        while (!StdIn.isEmpty()) {
+            tmp = StdIn.readString();
+            if (StdRandom.bernoulli(1.0 / (++count))) {
+                word = tmp;
+            }
+        }
+        StdOut.println(word);
+    }
+}
