9df29575ef74f33c3eaf2e9284326c1f3721e18e
Billy Yang <handsomexdd1024@gmail.com>
2023-03-17 18:48:37 +0800

    Assignment 1 completed

--- lab01/HelloGoodbye.java
+++ lab01/HelloGoodbye.java
@@ -1,6 +1,9 @@
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.StdOut;
 public class HelloGoodbye {
     public static void main(String[] args) {
-
+        String hello = "Hello " + args[0] + " and " + args[1] + ".";
+        String bye = "Goodbye " + args[1] + " and " + args[0] + ".";
+        StdOut.println(hello);
+        StdOut.println(bye);
     }
 }
--- lab01/HelloWorld.java
+++ lab01/HelloWorld.java
@@ -1,4 +1,4 @@
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.StdOut;
 public class HelloWorld {
     public static void main(String[] args) {
         StdOut.println("Hello, World");
