81dd68708143677b4240d4a474ea1acc46382880
Billy Yang <handsomexdd1024@gmail.com>
2023-03-20 09:25:38 +0800

    worked

--- hello/RandomWord.java
+++ hello/RandomWord.java
@@ -6,16 +6,6 @@ public class RandomWord {
     public static void main(String[] args) {
         String word = "", tmp;
         int count = 0;
-        //        while (true) {
-        //            try {
-        //                tmp = StdIn.readString();
-        //                if (StdRandom.bernoulli(1.0/(++count))) {
-        //                    word = tmp;
-        //                }
-        //            } catch (NoSuchElementException e) {
-        //                break;
-        //            }
-        //        }
         while (!StdIn.isEmpty()) {
             tmp = StdIn.readString();
             if (StdRandom.bernoulli(1.0 / (++count))) {
--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,21 +1,48 @@
 public class Percolation {
+    private boolean[][] grid;
+    private int size;
+
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException(
+                "Creating a percolation with negative size value"
+        );
+        size = n;
+        grid = new boolean[n][n];
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                grid[row][col] = false;
+            }
+        }
+    }
 
+    private boolean boundsCheck(int row, int col) {
+        return (row >= 1 && row <= size && col >= 1 && col <= size);
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-
+        if (!boundsCheck(row, col)) throw new IllegalArgumentException(
+                "Grid access out of bound."
+        );
+        if (!isOpen(row, col)) {
+            grid[row - 1][col - 1] = true;
+        }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-
+        if (!boundsCheck(row, col)) throw new IllegalArgumentException(
+                "Grid access out of bound."
+        );
+        return grid[row - 1][col - 1];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
+        if (!boundsCheck(row, col)) throw new IllegalArgumentException(
+                "Grid acces out of bound."
+        );
 
     }
 
