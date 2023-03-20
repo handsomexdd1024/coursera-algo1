678c8b179eb3811add6624a5726ba9c983094f5c
Billy Yang <me@motorscooter.run>
2023-03-20 17:11:10 +0800

    worked

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,49 +1,67 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
-    private boolean[][] grid;
+    private static int[][] direction = {
+            {1, 0},
+            {0, 1},
+            {-1, 0},
+            {0, -1}
+    };
+    private boolean[] grid;
+    private WeightedQuickUnionUF block;
     private int size;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException(
-                "Creating a percolation with negative size value"
+                "Initializing with a non-positive value of board size"
         );
         size = n;
-        grid = new boolean[n][n];
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                grid[row][col] = false;
-            }
+        grid = new boolean[n * n];
+        block = new WeightedQuickUnionUF(n * n);
+        for (int i = 0; i < n*n; i++) {
+            grid[i] = false;
         }
     }
 
-    private boolean boundsCheck(int row, int col) {
-        return (row >= 1 && row <= size && col >= 1 && col <= size);
+    private boolean safeBoundsCheck(int row, int col) {
+        return (row > 0 && row <= size && col > 0 && col <= size);
+    }
+
+    private void boundsCheck(int row, int col) {
+        if (!safeBoundsCheck(row, col)) {
+            throw new IllegalArgumentException(
+                    "Grid access out of bound."
+            );
+        }
+    }
+
+    private int loc2index(int row, int col) {
+        boundsCheck(row, col);
+        return ((row-1)*size+col-1);
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (!boundsCheck(row, col)) throw new IllegalArgumentException(
-                "Grid access out of bound."
-        );
+        boundsCheck(row, col);
         if (!isOpen(row, col)) {
-            grid[row - 1][col - 1] = true;
+            grid[loc2index(row, col)] = true;
+            for (int i = 0; i < 4; i++) {
+                if (
+                        safeBoundsCheck(row + direction[i][0], col + direction[i][1])
+                        && isOpen(row + direction[i][0], col + direction[i][1])
+                ) {
+                    block.union();
+                }
+            }
         }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        if (!boundsCheck(row, col)) throw new IllegalArgumentException(
-                "Grid access out of bound."
-        );
-        return grid[row - 1][col - 1];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        if (!boundsCheck(row, col)) throw new IllegalArgumentException(
-                "Grid acces out of bound."
-        );
-
     }
 
     // returns the number of open sites
